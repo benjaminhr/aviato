4ac1be3d7f43306bef81a75507ee3ca253e39f62
benjaminhr <robsonhbenjamin@gmail.com>
2025-02-16 11:47:58 +0200

    user agent

--- src/bot_server.py
+++ src/bot_server.py
@@ -56,6 +56,9 @@ ytdl_format_options = {
     "no_warnings": True,
     "default_search": "auto",
     "source_address": "0.0.0.0",
+    "http_headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    },
 }
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
